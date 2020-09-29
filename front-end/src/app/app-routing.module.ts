3f335913c998112e49bec7ac5324f52228dbc109
TungNguyen <17521236@gm.uit.edu.vn>
2020-09-30 02:24:21 +0700

    Frontend Lists and Tasks - 50%

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskManagerComponent } from './components/task-manager/task-manager.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TaskManagerComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
