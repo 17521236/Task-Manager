4266f80b48d8e33deac2b069a445c931851b221b
TungNguyen <17521236@gm.uit.edu.vn>
2020-11-29 19:59:44 +0700

    update readme file

--- src/app/services/web-request.service.ts
+++ src/app/services/web-request.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebRequestService {
 
-  readonly ROOT_URL = 'https://task-manager-api-0.herokuapp.com'
+  readonly ROOT_URL = environment.SERVER_URL;
   constructor(private http: HttpClient) { }
 
   get(uri: string) {
